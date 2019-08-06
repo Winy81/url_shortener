293d6d411c14e4338a1043394fbae4773d2e083c
Adam Nemeth <nemeth.richo@gmail.com>
2019-08-06 13:32:49 +0100

    create_short_link#with exist link test has been created. ('passed already')

--- spec/features/create_short_link_spec.rb
+++ spec/features/create_short_link_spec.rb
@@ -16,4 +16,22 @@ RSpec.feature "Create short link" do
 
   end
 
+  scenario "with exist link" do
+
+    visit "/"
+
+    fill_in "url_from_web", with: "https://guides.rubyonrails.org/"
+    click_button "Submit"
+
+    visit "/"
+
+    fill_in "url_from_web", with: "https://guides.rubyonrails.org/"
+    click_button "Submit"
+
+    expect(page).to have_content("Your original Url is: https://guides.rubyonrails.org/")
+    expect(page).to have_content("Your sortener Url is: http://localhost:3000/#{@short_url}")
+    expect(page).to have_content("The link is exist already")
+
+  end
+
 end
