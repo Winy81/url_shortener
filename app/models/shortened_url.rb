e9b47daa248875f52f7babb8b1f274deabdb1583
Adam Nemeth <nemeth.richo@gmail.com>
2019-08-06 12:11:44 +0100

    validates of original_url for check valid link syntax with regular expression

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,2 +1,5 @@
 class ShortenedUrl < ApplicationRecord
+  validates :original_url, presence: true
+  validates_format_of :original_url, :multiline => true, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  before_create :generate_short_url
 end
