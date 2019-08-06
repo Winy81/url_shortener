50d80b33cb003d00e2d8fc8fe9e8bc5eda6a4c64
Adam Nemeth <nemeth.richo@gmail.com>
2019-08-06 12:15:36 +0100

    generate_short_url before action method. The lenght of the link changable, thats why date_filter method added just in case if someone using short length of link to avoid the duplication

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -3,3 +3,20 @@ class ShortenedUrl < ApplicationRecord
   validates_format_of :original_url, :multiline => true, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   before_create :generate_short_url
 end
+
+def generate_short_url
+  url = ([*('a'..'z'),*('0'..'9')]).sample(8).join.date_filter
+  old_url = ShortenedUrl.where(short_url: url).last
+  if old_url.present?
+    self.generate_short_url
+  else
+    self.short_url = url
+  end
+end
+
+public
+
+def date_filter
+  date = Date.today.month.to_s
+  self << date
+end
