61556448bec5eb6d641b87c4c26699e773d34f55
Adam Nemeth <nemeth.richo@gmail.com>
2019-08-07 21:38:00 +0100

    syntax error correction

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -3,23 +3,23 @@ class ShortenedUrl < ApplicationRecord
   #regex validation for valid url, this version working with "http://" and "https://" but not with www
   validates_format_of :original_url, :multiline => true, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   before_create :generate_short_url
-end
+  before_create :date_filter
 
-#generate a 8 caracter long random sting used as short_url
-def generate_short_url
-  url = ([*('a'..'z'),*('0'..'9')]).sample(8).join.date_filter
-  old_url = ShortenedUrl.where(short_url: url).last
-  if old_url.present?
-    self.generate_short_url
-  else
-    self.short_url = url
+  #generate a 8 caracter long random sting used as short_url
+  def generate_short_url
+    url = ([*('a'..'z'),*('0'..'9')]).sample(8).join << date_filter
+    old_url = ShortenedUrl.where(short_url: url).last
+    if old_url.present?
+      self.generate_short_url
+    else
+      self.short_url = url
+    end
   end
-end
 
-public
 
-#add an extra or 2 extra caracters on the end of the short_url. depend on current month to help avoid duplication
-def date_filter
-  date = Date.today.month.to_s
-  self << date
+  #add an extra or 2 extra caracters on the end of the short_url. depend on current month to help avoid duplication
+  def date_filter
+    date = Date.today.month.to_s
+  end
+
 end
